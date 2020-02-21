a4b7bd3c3f4e744b7670b40470f27fdb1f969986
Nanda Kishor KS <nandakishorks2010@gmail.com>
2020-02-21 15:02:14 +0530

    Added billing, course, user models

---
+++ src/app/shared/models/course.ts
@@ -0,0 +1,14 @@
+export class Course {
+  $key: string;
+  courseId: number;
+  courseName: string;
+  courseCategory: string;
+  coursePrice: number;
+  courseDescription: string;
+  courseImageUrl: string;
+  courseAdded: number;
+  courseQuatity: number;
+  ratings: number;
+  favourite: boolean;
+  courseSeller: string;
+}
